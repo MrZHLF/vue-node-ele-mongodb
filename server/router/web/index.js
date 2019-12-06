ee35f417df0958179e008dfd1b0d32f69358db02
MrZHLF <480970024@qq.com>
2019-12-06 17:22:58 +0800

    移动端编写

--- server/router/web/index.js
+++ server/router/web/index.js
@@ -908,7 +908,7 @@ module.exports = app => {
 
   // 英雄详情
   router.get('/heroes/:id',async(req,res) =>{
-    const data = await Hero.findById(req.params.id).lean();
+    const data = await Hero.findById(req.params.id).populate('categories').lean();
     res.send(data)
   })
 
--- web/src/views/Hero.vue
+++ web/src/views/Hero.vue
@@ -10,6 +10,26 @@
         更多英雄 &gt;
       </router-link>
     </div>
+    <div class="top" :style="{'background-image':`url(${model.banner})`}">
+      <div class="info text-white p-3 h-100 d-flex flex-column jc-end">
+          <div class="fs-sm">{{model.title}}</div>
+          <h2 class="my-2">{{model.name}}</h2>
+          <div class="fs-sm">{{model.categories.map(v => v.name).join('/')}}</div>
+          <div class="d-flex jc-between pt-2">
+          <div class="scores d-flex ai-center" v-if="model.scores">
+            <span>难度</span>
+            <span class="badge bg-primary">{{model.scores.difficult}}</span>
+            <span>技能</span>
+            <span class="badge bg-blue-1">{{model.scores.skills}}</span>
+            <span>攻击</span>
+            <span class="badge bg-danger">{{model.scores.attack}}</span>
+            <span>生存</span>
+            <span class="badge bg-dark">{{model.scores.survive}}</span>
+          </div>
+          <router-link to="/" tag="span" class="text-grey fs-sm">皮肤: 2 &gt;</router-link>
+        </div>
+      </div>
+    </div> 
   </div>
 </template>
 <script>
@@ -34,3 +54,47 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+@import '../assets/scss/_variables.scss';
+.page-hero {
+  .top {
+    height: 50vw;
+    background: #fff no-repeat top center;
+    background-size: auto 100%;
+  }
+  .info {
+    background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1));
+    .scores {
+      .badge {
+        margin: 0 0.25rem;
+        display: inline-block;
+        width: 1rem;
+        height: 1rem;
+        line-height: 0.9rem;
+        text-align: center;
+        border-radius: 50%;
+        font-size: 0.6rem;
+        border: 1px solid rgba(255, 255, 255, 0.2);
+      }
+    }
+  }
+  .skills {
+    img.icon {
+      width: 70px;
+      height: 70px;
+      border: 3px solid map-get($colors, 'white');
+      &.active {
+        border-color:map-get($colors, 'primary');
+      }
+      border-radius: 50%;
+    }
+  }
+  .hero-items {
+    img.icon{
+      width: 45px;
+      height: 45px;
+      border-radius: 50%;
+    }
+  }
+}
+</style>
